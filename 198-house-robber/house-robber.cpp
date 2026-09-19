2a94d97549bdc2d458c4ebdad9f61ea74449bc70
ShubhamBothe <shubhambothe333@gmail.com>
2026-09-19 15:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (6.20%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int fun(vector<int>& nums, int i, int n, int free,
-            vector<vector<int>>& dp) {
+    int fun(vector<int>& a, int n, int i, int free, vector<vector<int>>& dp) {
         if (i == n)
             return 0;
-        if (dp[i][free] != -1) {
+
+        if (dp[i][free] != -1)
             return dp[i][free];
+
+        if (free == 0) {
+            return dp[i][free] = fun(a, n, i + 1, 1, dp);
         }
-        int c1 = 0;
-        if (free == 1) {
-            c1 = nums[i] + fun(nums, i + 1, n, 0, dp);
-        }
-        int c2 = fun(nums, i + 1, n, 1, dp);
+
+        int c1 = a[i] + fun(a, n, i + 1, 0, dp);
+        int c2 = fun(a, n, i + 1, 1, dp);
+
         return dp[i][free] = max(c1, c2);
     }
+      
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
-        return fun(nums, 0, n, 1, dp);
+        vector<vector<int>> dp(n);
+
+        for (int i = 0; i < n; i++) {
+            vector<int> t(2, -1);
+            dp[i] = t;
+        }
+
+        return fun(nums, n, 0, 1, dp);
     }
 };
